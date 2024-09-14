fec40a4aaf20302c86cc237e991c2f9d8dcb8e56
anusharebala <anusharebala@gmail.com>
2024-09-14 21:28:00 +0530

    added

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -12,4 +12,16 @@ public class Sample {
 		System.out.println(3);
 
 	}
+	private void method4() {
+		System.out.println(4);
+
+	}
+	private void method5() {
+		System.out.println(5);
+
+	}
+	private void method6() {
+		System.out.println(6);
+
+	}
 }
